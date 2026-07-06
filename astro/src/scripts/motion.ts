191e7f23f62f59c4072fdbc8ca0d11c1e372c33a
Kinomé <snarky-skuas0s@icloud.com>
2026-07-06 21:55:26 +0200

    loader and favicon

--- astro/src/scripts/motion.ts
+++ astro/src/scripts/motion.ts
@@ -1,12 +1,10 @@
 import { initCursor } from "./cursor";
-import { initLoader } from "./loader";
 import { initMenu } from "./menu";
 import { initPhotoLightbox } from "./photos";
 import { initRevealAnimations } from "./reveal";
 import { initSmoothScroll } from "./scroll";
 import { initTheme } from "./theme";
 
-initLoader();
 initSmoothScroll();
 initMenu();
 initTheme();
